86b407cc9314b7a5bde465eff23c180e1b63a8a3
Numan Tahir <numan.tahir@novatoresols.com>
2025-03-04 10:33:07 +0500

    update

--- routes/Users.js
+++ routes/Users.js
@@ -716,7 +716,7 @@ Users.put("/update", verifyToken, async (req, res) => {
     console.log("Stage-6");
     console.log(updatedFields);
     // Update user
-    const { data: updatedUser, error: updateError } = await db
+    const { data: updatedUser, error: updateError } = await db.supabase
   .from('users') // Ensure 'User' matches your actual table name
   .update(updatedFields) // Object with fields to update
   .match({ id: user_id }); // Correct way to filter records in Supabase
